4ec1c63b68e31b1de117f72f22ca6817c1c755e2
MadMaksio <maksymilian.karbasz@onet.pl>
2021-05-08 18:05:18 +0200

    *

--- src/com/company/Panel.java
+++ src/com/company/Panel.java
@@ -13,11 +13,11 @@ public class Panel extends JPanel {
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        ImageIcon tron =  new ImageIcon("src/com/company/pictures/sala_tronowa1.png");
-        ImageIcon sluga = new ImageIcon("src/com/company/pictures/sługa.png");
+        ImageIcon tron =  new ImageIcon("src/com/company/pixelpictures/Cezar49.png");
+        ImageIcon sluga = new ImageIcon("src/com/company/pixelpictures/pixil-frame-0 (3).png");
         g.setColor(Color.gray);
         g.drawImage(tron.getImage(),0, 0, 1200, 780, null );
-        g.drawImage(sluga.getImage(), 60, 450, 300, 300, null);
+        g.drawImage(sluga.getImage(), 60, 450, 350, 300, null);
         //System.out.println(g.getClipBounds().width + " "+g.getClipBounds().height);
         /*for(int i = 0; i < 64; i++){
             g.drawLine(i*20,0,i*20,479);
